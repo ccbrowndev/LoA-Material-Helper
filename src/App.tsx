e7e5acc3b542fd13f84d46bb7bc8353582aa3f39
Jack <oportojack@gmail.com>
2022-08-07 22:11:35 -0400

    Add footer.tsx
    Adds link to Github project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import Divider from "./components/divider";
-import Settings from "./components/latest";
+import Settings from "./components/settings";
+import Footer from "./components/footer";
 
-function App(): JSX.Element {
+export default function App(): JSX.Element {
   return (
     <div className='max-w-5xl mx-auto px-4 pt-2 sm:px-6 lg:px-8'>
       <Settings />
       <Divider />
+      <Footer />
     </div>
   );
 }
-
-export default App;
