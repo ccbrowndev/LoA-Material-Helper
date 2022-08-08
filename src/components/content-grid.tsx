a3d851d7c75ae8052ca32dee35914248dac3684a
Chris Brown <46230726+ccbrowndev@users.noreply.github.com>
2022-08-08 03:52:12 -0400

    change the roster column headers to inputs

--- src/components/content-grid.tsx
+++ src/components/content-grid.tsx
@@ -2,12 +2,25 @@ export default function ContentGrid() {
   return (
     <details className='max-w-5xl mx-auto px-4 pt-2 sm:px-6 lg:px-8 text-white text-center'>
       <summary className='text-4xl pb-5'>Roster</summary>
-      <ul role='list' className='grid grid-cols-5 gap-6'>
-        <div>Item Level: INPUT FIELD</div>
-        <div>x NUM VALUE</div>
-        <div>Rested?</div>
-        <div>Toggle for Rested</div>
-        <div>Add character</div>
+      <ul role='list' className='grid grid-cols-5 gap-6 pb-5'>
+        <div>
+          <input type="text" placeholder="Item Level" />
+        </div>
+        <div>
+          <input type="text" className="w-12" placeholder="#" />
+        </div>
+        <div>
+          <label>
+            Rested?
+            <input type="checkbox" className="mx-4" />
+          </label>
+        </div>
+        <div>
+          <label className="border">
+            +
+            <input type="button" />
+          </label>
+        </div>
       </ul>
     </details>
   );
