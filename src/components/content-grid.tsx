b49b4e6f62db95a0e5728629744b011af42053c2
Chris Brown <46230726+ccbrowndev@users.noreply.github.com>
2022-08-07 23:53:44 -0400

    change roster to details tag to add collapsibility

--- src/components/content-grid.tsx
+++ src/components/content-grid.tsx
@@ -1,9 +1,7 @@
 export default function ContentGrid() {
   return (
-    <div className='max-w-5xl mx-auto px-4 pt-2 sm:px-6 lg:px-8 text-white text-center'>
-      <div className='pb-5'>
-        <span className='text-4xl'>Roster</span>
-      </div>
+    <details className='max-w-5xl mx-auto px-4 pt-2 sm:px-6 lg:px-8 text-white text-center'>
+      <summary className='text-4xl pb-5'>Roster</summary>
       <ul role='list' className='grid grid-cols-5 gap-6'>
         <div>Item Level: INPUT FIELD</div>
         <div>x NUM VALUE</div>
@@ -11,6 +9,6 @@ export default function ContentGrid() {
         <div>Toggle for Rested</div>
         <div>Add character</div>
       </ul>
-    </div>
+    </details>
   );
 }
