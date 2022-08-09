d0c6f1fe5e9bc7d1288814e05cd0337f701d0bd5
Chris Brown <46230726+ccbrowndev@users.noreply.github.com>
2022-08-09 02:44:27 -0400

    clean up settings.tsx and add goals.tsx plus call goals section in App.tsx

---
+++ src/components/goals.tsx
@@ -0,0 +1,22 @@
+export default function Goals() {
+    return (
+        <div className="pb-6 sm:px-6 lg:px-8 h-auto text-white text-center">
+            <details open>
+                <summary className="text-2xl font-bold tracking-tight hover:cursor-pointer">Goals</summary>
+                <div>
+                    <label>
+                        Select a goal:
+                        <select>
+                            <option value="20wep21">1340 Weapon +20 to +21</option>
+                            <option value="1340alt1370">1340 Alt to 1370</option>
+                            <option value="custom">Custom goal</option>
+                        </select>
+                    </label>
+                    <div className="invisible">
+                        {/*todo: add fields for inputting a custom goal and naming it*/}
+                    </div>
+                </div>
+            </details>
+        </div>
+    )
+}
