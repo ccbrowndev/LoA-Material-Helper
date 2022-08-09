71dc700c130b00b0fa0e9f3168748c74298a0463
Chris Brown <46230726+ccbrowndev@users.noreply.github.com>
2022-08-09 02:22:52 -0400

    make section summaries more consistently styled

--- src/components/content-grid.tsx
+++ src/components/content-grid.tsx
@@ -1,7 +1,7 @@
 export default function ContentGrid() {
   return (
     <details className='max-w-5xl mx-auto px-4 pt-2 sm:px-6 lg:px-8 text-white text-center'>
-      <summary className='text-4xl pb-5'>Roster</summary>
+      <summary className='text-2xl font-bold tracking-tight hover:cursor-pointer pb-5'>Roster</summary>
       <ul role='list' className='grid grid-cols-5 gap-6 pb-5'>
         <div>
           <input type="text" placeholder="Item Level" />
