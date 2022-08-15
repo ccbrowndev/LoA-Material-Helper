7b2fdb7212047661c45f5b1e4bdd0df46fb09298
Jack <oportojack@gmail.com>
2022-08-15 18:42:51 -0400

    Add conditional color styles to iLevel

--- src/components/userCharacter.tsx
+++ src/components/userCharacter.tsx
@@ -6,11 +6,20 @@ export default function UserCharacter(character: Character): JSX.Element {
       <div className='flex-1 truncate w-fit border-slate-700 border-2 p-1.5 -m-1.5 rounded-lg'>
         <div className='flex items-center space-x-1'>
           {" "}
-          <span className='flex-shrink-0 inline-block px-2 py-0.5 text-black text-xs font-medium bg-amber-300 rounded-full'>
+          <span
+            className={`flex-shrink-0 inline-block px-2 py-0.5 text-white text-xs font-medium rounded-full
+            ${character.iLevel.number <= 1355 ? "bg-purple-500" : ""}
+            ${character.iLevel.number >= 1445 ? "bg-orange-500" : ""}
+            ${
+              character.iLevel.number > 1355 && character.iLevel.number < 1445
+                ? "bg-amber-500 text-white"
+                : ""
+            }`}
+          >
             {character.iLevel.number}
           </span>
           <span
-            className={`flex-shrink-0 inline-block px-1.5 py-0.5 text-green-800 text-xs font-medium  ${
+            className={`flex-shrink-0 inline-block px-1.5 py-0.5 text-black text-xs font-medium  ${
               character.rested ? "bg-green-100" : "bg-red-100"
             } rounded-full`}
           >
