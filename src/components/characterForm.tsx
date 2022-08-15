9cf2304e6d5f2b96674393635f8c1734784cae87
Jack <oportojack@gmail.com>
2022-08-15 17:41:01 -0400

    Add character cards
    Generic styled rows -> generic styled cards 👍
    Add button does not work 👍

--- src/components/characterForm.tsx
+++ src/components/characterForm.tsx
@@ -1,3 +1,4 @@
+import { MinusIcon, PlusIcon } from "@heroicons/react/solid";
 import { useState } from "react";
 import { Character } from "../types/character";
 import { convertToLevel } from "../utils/levelConverter";
@@ -28,13 +29,28 @@ export function CharacterForm() {
     );
   };
 
+  //This function will pass in the character array and the id of the character to have their amount increased by 1
+  const handleIncrease = (id: number) => {
+    setCharacterArray(
+      characterArray.map((character) => {
+        if (character.id === id) {
+          return {
+            ...character,
+            amount: character.amount + 1,
+          };
+        }
+        return character;
+      })
+    );
+  };
+
   //This function will generate a unique random id for each character.
   const generateId = () => {
     return Math.floor(Math.random() * 1000000);
   };
 
   return (
-    <>
+    <div>
       <form
         onSubmit={handleSubmit}
         className='flex flex-row justify-around items-center pb-2'
@@ -81,30 +97,50 @@ export function CharacterForm() {
           </label>
         </div>
       </form>
-      <ul role='list' className='grid grid-row-5 gap-2 pb-5'>
+      <ul
+        role='list'
+        className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'
+      >
         {characterArray.map((character: Character) => (
-          <li className='flex flex-row justify-around items-center py-2 border rounded-md bg-slate-600 hover:bg-slate-800'>
+          <li
+            key={character.id}
+            className='col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200 overflow-hidden'
+          >
             <UserCharacter
-              key={character.id}
               id={character.id}
               iLevel={character.iLevel}
               amount={character.amount ? character.amount : 1}
               rested={character.rested}
             />
-            <span>
+            <div className='-mt-px flex divide-x divide-gray-300'>
               <button
-                onClick={() => {
-                  handleDelete(character.id);
-                }}
-                className='hover:cursor-pointer '
+                onClick={() => handleDelete(character.id)}
+                className='w-0 flex-1 flex cursor-pointer'
               >
-                [-]
+                <div className='relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500 hover:bg-slate-300'>
+                  <MinusIcon
+                    className='w-5 h-5 text-gray-600'
+                    aria-hidden='true'
+                  />
+                  <span className='ml-3'>Remove</span>
+                </div>
               </button>
-              <button className='hover:cursor-pointer '>[+]</button>
-            </span>
+              <button
+                onClick={() => handleIncrease(character.id)}
+                className='-ml-px w-0 flex-1 flex cursor-pointer'
+              >
+                <div className='relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-600 hover:bg-slate-300'>
+                  <PlusIcon
+                    className='w-5 h-5 text-gray-600'
+                    aria-hidden='true'
+                  />
+                  <span className='ml-3'>Add</span>
+                </div>
+              </button>
+            </div>
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 }
