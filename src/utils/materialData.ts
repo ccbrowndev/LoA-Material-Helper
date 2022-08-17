4a47848b483ac87de341b37fb4757e02fd3fef48
ccbrowndev <46230726+ccbrowndev@users.noreply.github.com>
2022-08-17 00:44:11 -0400

    add an initial version of material data as array of objects

---
+++ src/utils/materialData.ts
@@ -0,0 +1,102 @@
+const object1325 = {
+    chaosReds: 90,
+    chaosBlues: 273,
+    chaosLeaps: 5,
+    shards: 2725,
+    guardianReds: 40,
+    guardianBlues: 130,
+    guardianLeaps: 8,
+}
+const object1340 = {
+    chaosReds: 105,
+    chaosBlues: 307,
+    chaosLeaps: 6,
+    shards: 3077,
+    guardianReds: 50,
+    guardianBlues: 160,
+    guardianLeaps: 10,
+}
+const object1355 = {
+    chaosReds: 112,
+    chaosBlues: 328,
+    chaosLeaps: 7,
+    shards: 3560,
+    guardianReds: 50,
+    guardianBlues: 160,
+    guardianLeaps: 10,
+}
+const object1370 = {
+    chaosReds: 132,
+    chaosBlues: 365,
+    chaosLeaps: 7,
+    shards: 3875,
+    guardianReds: 70,
+    guardianBlues: 300,
+    guardianLeaps: 7,
+}
+const object1385 = {
+    chaosReds: 134,
+    chaosBlues: 403,
+    chaosLeaps: 8,
+    shards: 4283,
+    guardianReds: 80,
+    guardianBlues: 250,
+    guardianLeaps: 9,
+}
+const object1400 = {
+    chaosReds: 147,
+    chaosBlues: 415,
+    chaosLeaps: 10,
+    shards: 4500,
+    guardianReds: 80,
+    guardianBlues: 250,
+    guardianLeaps: 9,
+}
+const object1415 = {
+    chaosReds: 158,
+    chaosBlues: 427,
+    chaosLeaps: 11,
+    shards: 4800,
+    guardianReds: 90,
+    guardianBlues: 300,
+    guardianLeaps: 11,
+}
+const object1445 = {
+    chaosReds: 166,
+    chaosBlues: 491,
+    chaosLeaps: 12,
+    shards: 5177,
+    guardianReds: 90,
+    guardianBlues: 300,
+    guardianLeaps: 11,
+}
+const object1460 = {
+    chaosReds: 166,
+    chaosBlues: 491,
+    chaosLeaps: 11,
+    shards: 5177,
+    guardianReds: 100,
+    guardianBlues: 350,
+    guardianLeaps: 13,
+}
+const object1475 = {
+    chaosReds: 174,
+    chaosBlues: 516,
+    chaosLeaps: 12,
+    shards: 5800,
+    guardianReds: 100,
+    guardianBlues: 350,
+    guardianLeaps: 13,
+}
+
+export const materials: Array<Object> = [object1325, object1340, object1355, object1370, object1385, object1400, object1415, object1445, object1460, object1475]
+
+// const map1325 : Map<string, number> = new Map<string, number>([
+//     ["chaosReds", 90],
+//     ["chaosBlues", 273],
+//     ["chaosLeaps", 5],
+//     ["shards", 2725],
+//     ["guardianReds", 40],
+//     ["guardianBlues", 130],
+//     ["guardianLeaps", 8],
+// ])
