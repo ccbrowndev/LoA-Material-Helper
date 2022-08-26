ffa0bfd911e51fd6f022769d14739a60954980f1
ccbrowndev <46230726+ccbrowndev@users.noreply.github.com>
2022-08-26 16:18:56 -0400

    add Goal type and goalData
    Co-authored-by: Jack <60209373+OpoJack@users.noreply.github.com>

--- src/components/goals.tsx
+++ src/components/goals.tsx
@@ -1,9 +1,37 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CharacterContext } from "../App";
-import AnimatingNumber from "../utils/animating-number";
+import { Character } from "../types/character";
+import { Goal } from "../types/goal";
+import { goalData } from "../utils/goalData";
 
 export default function Goals() {
+  const defaultGoal : Goal = {name: 'custom',
+  redsRequired: 0,
+  bluesRequired: 0,
+  leapsRequired: 0,
+  shardsRequired: 0,}
+  
   const { characterArray, setCharacterArray } = useContext(CharacterContext);
+  const [goal, setGoal] = useState<Goal>(defaultGoal);
+  const [eta, setEta] = useState();
+
+  const calcETA = (g: Goal, charList : Array<Character>) => {
+    /* Pseudo-Code
+    Find the highest value between dividing goal amount by roster total of each material 
+    ETA = Ceiling(theHighestValue)
+    */
+  }
+
+  const handleGoalSelection = (event: { target: HTMLSelectElement; }) => {
+    for (const g in goalData) {
+      if (g === event.target.value) {
+        setGoal(goalData.get(g) || defaultGoal);        
+      };
+    };
+
+    // setEta(calcEta(goal, characterArray))
+
+  };
 
   return (
     <div className='pb-6 sm:px-6 lg:px-8 h-auto text-white text-center'>
@@ -14,7 +42,7 @@ export default function Goals() {
         <div>
           <label className=''>
             Select a goal
-            <select className='text-black'>
+            <select className='text-black' value={goal.name} onChange={handleGoalSelection}>
               {/* Variables:  */}
               <option value='20wep21'>1340 Weapon +20 to +21</option>
               <option value='1340alt1370'>1340 Alt to 1370</option>
