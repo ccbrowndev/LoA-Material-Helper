c9b42687dcc7bef9a7da5ac3aaee683c7d88d125
ccbrowndev <46230726+ccbrowndev@users.noreply.github.com>
2022-09-03 19:50:27 -0400

    add basic persistence upon adding a character
    Co-authored-by: Jack <60209373+OpoJack@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Divider from "./components/divider";
-import Settings from "./components/settings";
-import Footer from "./components/footer";
-import Goals from "./components/goals";
-import Roster from "./components/roster";
-import { Character } from "./types/character";
-import { createContext, useState } from "react";
+import Divider from './components/divider';
+import Settings from './components/settings';
+import Footer from './components/footer';
+import Goals from './components/goals';
+import Roster from './components/roster';
+import { Character } from './types/character';
+import { createContext, useEffect, useState } from 'react';
 
 export const CharacterContext = createContext({
   characterArray: Array<Character>(),
@@ -13,14 +13,28 @@ export const CharacterContext = createContext({
 
 export default function App(): JSX.Element {
   const [characterArray, setCharacterArray] = useState<Character[]>([]);
+
+  /*
+  Loads the user's characters if they exist in local storage
+  useEffect is given an empty dependency to run only 1 time
+  This prevents an infinite loop
+  */
+  useEffect(() => {
+    const getLocalCharResult: string =
+      localStorage.getItem('localCharacters') || '';
+    if (getLocalCharResult !== '') {
+      setCharacterArray(JSON.parse(getLocalCharResult));
+    }
+  }, []);
+
   return (
-    <div className='max-w-5xl mx-auto px-4 pt-2 sm:px-2 lg:px-8'>
+    <div className="max-w-5xl mx-auto px-4 pt-2 sm:px-2 lg:px-8">
       <Settings />
       <Divider />
       <CharacterContext.Provider value={{ characterArray, setCharacterArray }}>
-          <Roster />
-          <Divider />
-          <Goals />
+        <Roster />
+        <Divider />
+        <Goals />
       </CharacterContext.Provider>
       <Footer />
     </div>
--- src/components/characterForm.tsx
+++ src/components/characterForm.tsx
@@ -1,18 +1,19 @@
-import { useRef, useState, useEffect, useContext } from "react";
-import { Character } from "../types/character";
-import { convertToLevel } from "../utils/levelConverter";
-import UserCharacter from "./userCharacter";
-import autoAnimate from "@formkit/auto-animate";
-import { CharacterContext } from "../App";
-import { materialData } from "../utils/materialData";
+import { useRef, useState, useEffect, useContext } from 'react';
+import { Character } from '../types/character';
+import { convertToLevel } from '../utils/levelConverter';
+import UserCharacter from './userCharacter';
+import autoAnimate from '@formkit/auto-animate';
+import { CharacterContext } from '../App';
+import { materialData } from '../utils/materialData';
+import { addToLocalStorage } from '../utils/persistence';
 
 export const CharacterForm = () => {
   const { characterArray, setCharacterArray } = useContext(CharacterContext);
 
-  const [level, setLevel] = useState("");
-  const [amount, setAmount] = useState("1");
+  const [level, setLevel] = useState('');
+  const [amount, setAmount] = useState('1');
   const [rested, setRested] = useState(false);
-  
+
   const parent = useRef(null);
 
   function getMaterials(charLevel: number) {
@@ -30,9 +31,15 @@ export const CharacterForm = () => {
     const blues = materials.chaosBlues + materials.guardianBlues;
     const chaosLeaps = materials.chaosLeaps;
     const guardianLeaps = materials.guardianLeaps;
-    const shards = materials.shards; 
+    const shards = materials.shards;
 
-    return {reds: reds, blues: blues, chaosLeaps: chaosLeaps, guardianLeaps: guardianLeaps, shards: shards};
+    return {
+      reds: reds,
+      blues: blues,
+      chaosLeaps: chaosLeaps,
+      guardianLeaps: guardianLeaps,
+      shards: shards,
+    };
   }
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
@@ -40,30 +47,31 @@ export const CharacterForm = () => {
     const parsedLevel = convertToLevel(parseInt(level));
     const cid = generateId();
     const mats = getMaterials(parsedLevel.number);
-    setCharacterArray([
-      ...characterArray,
-      {
-        iLevel: parsedLevel,
-        amount: parseInt(amount),
-        rested: rested,
-        id: cid,
-        isTargeted: characterArray.length === 0 ? true : false,
-        //Characters operating on rested bonus earn 2/3rds the mats, floor function applied to keep display whole numbers
-        totalMaterials: rested ? {
-          totalReds: Math.floor(mats.reds * (2/3)),
-          totalBlues: Math.floor(mats.blues * (2/3)),
-          guardianLeaps: Math.floor(mats.guardianLeaps * (2/3)),
-          chaosLeaps: Math.floor(mats.chaosLeaps * (2/3)),
-          totalShards: Math.floor(mats.shards * (2/3)),
-        } : {
-          totalReds: mats.reds,
-          totalBlues: mats.blues,
-          guardianLeaps: mats.guardianLeaps,
-          chaosLeaps: mats.chaosLeaps,
-          totalShards: mats.shards,
-        },
-      },
-    ]);
+    const charToBeAdded: Character = {
+      iLevel: parsedLevel,
+      amount: parseInt(amount),
+      rested: rested,
+      id: cid,
+      isTargeted: characterArray.length === 0 ? true : false,
+      //Characters operating on rested bonus earn 2/3rds the mats, floor function applied to keep display whole numbers
+      totalMaterials: rested
+        ? {
+            totalReds: Math.floor(mats.reds * (2 / 3)),
+            totalBlues: Math.floor(mats.blues * (2 / 3)),
+            guardianLeaps: Math.floor(mats.guardianLeaps * (2 / 3)),
+            chaosLeaps: Math.floor(mats.chaosLeaps * (2 / 3)),
+            totalShards: Math.floor(mats.shards * (2 / 3)),
+          }
+        : {
+            totalReds: mats.reds,
+            totalBlues: mats.blues,
+            guardianLeaps: mats.guardianLeaps,
+            chaosLeaps: mats.chaosLeaps,
+            totalShards: mats.shards,
+          },
+    };
+    addToLocalStorage('localCharacters', charToBeAdded);
+    setCharacterArray([...characterArray, charToBeAdded]);
   };
 
   useEffect(() => {
@@ -79,70 +87,70 @@ export const CharacterForm = () => {
     <div>
       <form
         onSubmit={handleSubmit}
-        className='flex flex-row justify-center items-center pb-5 space-x-7 sm:space-x-1'
+        className="flex flex-row justify-center items-center pb-5 space-x-7 sm:space-x-1"
       >
-        <div className='relative border border-gray-300 rounded-md p-2 shadow-sm'>
+        <div className="relative border border-gray-300 rounded-md p-2 shadow-sm">
           <label
-            htmlFor='Level'
-            className='absolute -top-2 left-2 -mt-px inline-block px-1 bg-slate-800 text-xs font-medium'
+            htmlFor="Level"
+            className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-slate-800 text-xs font-medium"
           >
             iLevel
           </label>
           <input
-            type='text'
-            name='level'
-            id='level'
-            className='block w-full border-0 p-1 text-white bg-slate-800 focus:bg-slate-700 placeholder-gray-300 focus:ring-0 sm:text-sm rounded-sm'
-            placeholder='1325+'
+            type="text"
+            name="level"
+            id="level"
+            className="block w-full border-0 p-1 text-white bg-slate-800 focus:bg-slate-700 placeholder-gray-300 focus:ring-0 sm:text-sm rounded-sm"
+            placeholder="1325+"
             value={level}
-            onChange={(e) => setLevel(e.target.value.replace(/[^\d.]/g, ""))}
+            onChange={(e) => setLevel(e.target.value.replace(/[^\d.]/g, ''))}
           />
         </div>
-        <div className='relative border border-gray-300 rounded-md p-2 shadow-sm'>
+        <div className="relative border border-gray-300 rounded-md p-2 shadow-sm">
           <label
-            htmlFor='name'
-            className='absolute -top-2 left-2 -mt-px inline-block px-1 bg-slate-800 text-xs font-medium'
+            htmlFor="name"
+            className="absolute -top-2 left-2 -mt-px inline-block px-1 bg-slate-800 text-xs font-medium"
           >
             Amount
           </label>
           <input
-            type='text'
-            name='amount'
-            id='amount'
-            className='block w-full border-0 p-1 text-white bg-slate-800 focus:bg-slate-700 placeholder-gray-300 focus:ring-0 sm:text-sm rounded-sm'
-            placeholder='#'
+            type="text"
+            name="amount"
+            id="amount"
+            className="block w-full border-0 p-1 text-white bg-slate-800 focus:bg-slate-700 placeholder-gray-300 focus:ring-0 sm:text-sm rounded-sm"
+            placeholder="#"
             value={amount}
             onChange={(e) => {
-              const result = e.target.value.replace(/\D/g, "");
+              const result = e.target.value.replace(/\D/g, '');
               setAmount(result);
             }}
           />
         </div>
-        <div className='p-1'>
+        <div className="p-1">
           <label>
             Rested?
             <input
-              className='ml-1'
-              type='checkbox'
-              about='Rested'
+              className="ml-1"
+              type="checkbox"
+              about="Rested"
               onChange={(e) => setRested(e.target.checked)}
             />
           </label>
         </div>
-        <div className='border px-3 py-1 rounded-xl bg-slate-800 hover:bg-slate-600 text-white font-bold'>
+        <div className="border px-3 py-1 rounded-xl bg-slate-800 hover:bg-slate-600 text-white font-bold">
           <label>
             <input
-              type='submit'
-              className='hover:cursor-pointer'
-              value='Submit'
+              type="submit"
+              className="hover:cursor-pointer"
+              value="Submit"
             />
           </label>
         </div>
       </form>
       <ul
         ref={parent}
-        role='list'
-        className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'
+        role="list"
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
       >
         {characterArray.map((character: Character) => (
           <UserCharacter
