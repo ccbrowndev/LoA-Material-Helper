a50128dee5034870431938b57187295c3186fee2
ccbrowndev <46230726+ccbrowndev@users.noreply.github.com>
2022-09-03 20:28:00 -0400

    fix addToLocalStorage logic

--- src/utils/persistence.tsx
+++ src/utils/persistence.tsx
@@ -5,18 +5,17 @@ import { Goal } from '../types/goal';
 //or creates a new array in localStorage if one doesn't exist
 export function addToLocalStorage(key: string, value: Character | Goal) {
   const currentStringValue = localStorage.getItem(key);
-  const currentObjectValue = JSON.parse(currentStringValue || '');
 
-  try {
+  //If there is nothing at the provided key, create it
+  //Otherwise add to the existing array
+  if (!currentStringValue) {
+    localStorage.setItem(key, JSON.stringify([value]));
+  } else {
+    const currentObjectValue = JSON.parse(currentStringValue || '');
     if (currentObjectValue !== '') {
       const newObjectValue = [...currentObjectValue, value];
       const newStringValue = JSON.stringify(newObjectValue);
       localStorage.setItem(key, newStringValue);
-    } else {
-      const initialArray: Array<Character | Goal> = [value];
-      localStorage.setItem(key, JSON.stringify(initialArray));
     }
-  } catch (error) {
-    console.log('Error! ', error);
   }
 }
