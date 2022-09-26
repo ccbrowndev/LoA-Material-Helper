7a20bbb279fede9cd8eeb6758815ef16c14f8a74
Jack <60209373+OpoJack@users.noreply.github.com>
2022-09-26 17:47:23 -0400

    Add app explanation/guide to help section
    Probably needs an image or two

--- src/components/help.tsx
+++ src/components/help.tsx
@@ -1,6 +1,7 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
+import { InformationCircleIcon } from '@heroicons/react/solid';
 
 export default function Help({
   isShown,
@@ -37,18 +38,53 @@ export default function Help({
             >
               <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6'>
                 <div>
-                  <div className='mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100'></div>
+                  <div className='mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-600'>
+                    <InformationCircleIcon color='white' />
+                  </div>
                   <div className='mt-3 text-center sm:mt-5'>
                     <Dialog.Title
                       as='h3'
                       className='text-lg font-medium leading-6 text-gray-900'
                     >
-                      Payment successful
+                      How to use
                     </Dialog.Title>
                     <div className='mt-2'>
-                      <p className='text-sm text-gray-500'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Consequatur amet labore.
+                      <p className='text-sm text-gray-700'>
+                        You can enter any iLevel number but the lower bound is
+                        <b> 1325</b>, upper bound is <b>1475</b>.
+                        <br />
+                        In-between values are rounded down to the closest
+                        interval.
+                        <br />
+                        <b>
+                          Material values are an average of resources obtainable
+                          daily by the iLevel value.
+                        </b>
+                        <br />
+                        Increase the amount of characters to account for others
+                        in the roster at that iLevel interval.
+                      </p>
+                    </div>
+                    <div className='mt-5'>
+                      <p className='text-sm text-left text-gray-700'>
+                        <ul>
+                          <li>
+                            1. Add your characters to the roster via the
+                            provided form.
+                          </li>
+                          <li>
+                            2. Select a goal target to funnel resources into.
+                          </li>
+                          <li>3. Select a goal or create your own.</li>
+                          <li className='indent-4'>
+                            3a. If you create your own goal, input your
+                            currently held materials first.
+                          </li>
+                          <li>
+                            4. App will output the estimated days it'll take to
+                            complete your goal.
+                          </li>
+                        </ul>
                       </p>
                     </div>
                   </div>
@@ -56,10 +92,10 @@ export default function Help({
                 <div className='mt-5 sm:mt-6'>
                   <button
                     type='button'
-                    className='inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm'
+                    className='inline-flex w-full justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:text-sm'
                     onClick={() => setIsShown(false)}
                   >
-                    Go back to dashboard
+                    Go back
                   </button>
                 </div>
               </Dialog.Panel>
